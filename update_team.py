a954b3bae8cf4825220cbb988dd59e23f253a658
RobPasMue <roberto.pastormuela@ansys.com>
2022-05-30 08:48:33 +0200

    F-string impl. for printing mechanism

--- update_team.py
+++ update_team.py
@@ -40,19 +40,17 @@ g = github.Github(MY_PAT)
 
 # Let us get the org
 g_org = g.get_organization(MY_ORG)
-print("Connecting to the " + g_org.name + " organization...")
+print(f"Connecting to the {g_org.name} organization...")
 
 # Let us get the users
 g_org_members = g_org.get_members()
-print("Retrieving members... Total count: " + str(g_org_members.totalCount))
+print(f"Retrieving members... Total count: {g_org_members.totalCount}")
 
 # Now, let us get the users of our all-members team
 g_team = g_org.get_team_by_slug("all-members")
 g_team_members = g_team.get_members()
 print(
-    "Retrieving the 'all-members' team members... Total count: "
-    + str(g_team_members.totalCount)
-)
+    f"Retrieving the 'all-members' team members... Total count: {g_team_members.totalCount}")
 
 # In case there are missing members... let us add them!
 if g_team_members.totalCount != g_org_members.totalCount:
@@ -66,7 +64,7 @@ if g_team_members.totalCount != g_org_members.totalCount:
     for g_org_member in g_org_members:
         # Check if the user is a member...
         if not g_team.has_in_members(g_org_member):
-            print(g_org_member.login + " should be added!")
+            print(f"{g_org_member.login} should be added!")
             users_to_add.add(g_org_member)
 
         # Check if we have identified all missing users
@@ -74,11 +72,11 @@ if g_team_members.totalCount != g_org_members.totalCount:
             break
 
     # Show how many users will be added
-    print("Users to be added: " + str(len(users_to_add)))
+    print(f"Users to be added: {len(users_to_add)}")
 
     # Adding missing members to team
     for user in users_to_add:
         g_team.add_to_members(user)
-        print(user.login + " has been added!")
+        print(f"{user.login} has been added!")
 else:
     print("No users missing! All up-to-date.")
